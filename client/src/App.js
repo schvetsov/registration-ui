6e4b8f41189144a6f06505565fee0b4c49d6983c
Stan Chvetsov <sac08xyz@gmail.com>
2019-04-05 17:16:34 -0400

    Add form validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import axios from 'axios';
+import { Route, Link } from 'react-router-dom';
+import Submitted from './pages/Submitted';
 
 class App extends Component {
 
@@ -16,10 +18,12 @@ class App extends Component {
       homeState: '',
       zip: '',
       phone: '',
-      email: ''
+      email: '',
+      registered: false
     }
     this.onSubmit = this.onSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    // this.componentDidMount = this.componentDidMount.bind(this);
   }
 
   handleChange = name => event => {
@@ -27,20 +31,20 @@ class App extends Component {
   }
 
   onSubmit() {
-    axios.post('/register', {
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      npi: this.state.npi,
-      address1: this.state.address1,
-      address2: this.state.address2,
-      city: this.state.city,
-      homeState: this.state.homeState,
-      zip: this.state.zip,
-      phone: this.state.phone,
-      email: this.state.email
-    })
-    .then(res => console.log(res))
-    .catch(err => console.log(err));
+      axios.post('/register', {
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        npi: this.state.npi,
+        address1: this.state.address1,
+        address2: this.state.address2,
+        city: this.state.city,
+        homeState: this.state.homeState,
+        zip: this.state.zip,
+        phone: this.state.phone,
+        email: this.state.email
+      })
+      .then(this.setState({registered: true}))
+      .catch(err => console.log(err));
   }    
 
   componentDidMount() {
@@ -54,8 +58,10 @@ class App extends Component {
             if (form.checkValidity() === false) {
               event.preventDefault();
               event.stopPropagation();
+              console.log('propagating')
             }
             form.classList.add('was-validated');
+            console.log('validated')
           }, false);
         });
       }, false);
@@ -65,107 +71,113 @@ class App extends Component {
     return (
       <div>
         <div className="pos-f-t">
-          <div className="collapse" id="navbarToggleExternalContent">
-            <div className="bg-dark p-4">
-              <h5 className="text-white h4">Collapsed content</h5>
-              <span className="text-muted">Toggleable via the navbar brand.</span>
+          <nav className="navbar navbar-dark bg-dark">
+            <div className="container">
+              <a className="navbar-brand" onClick={() => this.setState({registered: false})}>
+                <img className="logo" src={require('./availity.png')} />
+              </a>
+              <div className="navbar-text">
+                Registration
+              </div>
             </div>
-          </div>
-          <nav class="navbar navbar-dark bg-dark">
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+            
           </nav>
         </div>
-        <form className="needs-validation App" noValidate>
-          <div className="form-row">
-            <div className="col-md-4 mb-3">
-              <label htmlFor="validationCustom01">First name</label>
-              <input type="text" className="form-control" id="validationCustom01" placeholder="First name" value={this.state.firstName} onChange={this.handleChange('firstName')} required/>
-              <div className="valid-feedback">
-                Looks good!
-              </div>
-              <div className="invalid-feedback">
-                Please provide a valid first name.
-              </div>
-            </div>
-            <div className="col-md-4 mb-3">
-              <label htmlFor="validationCustom02">Last name</label>
-              <input type="text" className="form-control" id="validationCustom02" placeholder="Last name" value={this.state.lastName} onChange={this.handleChange('lastName')} required/>
-              <div className="valid-feedback">
-                Looks good!
-              </div>
-              <div className="invalid-feedback">
-                Please provide a valid last name.
-              </div>
-            </div>
-            <div className="col-md-4 mb-3">
-              <label htmlFor="validationCustom03">NPI</label>
-              <input type="text" className="form-control" id="validationCustom02" placeholder="NPI" value={this.state.npi} onChange={this.handleChange('npi')} required/>
-              <div className="valid-feedback">
-                Looks good!
-              </div>
-              <div className="invalid-feedback">
-                Please provide a valid NPI.
-              </div>
-            </div>
-          </div>
-          <div className="form-row">
-              <div className="col-md-6 mb-3">
-              <label htmlFor="validationCustom03">Address 1</label>
-                <input type="text" className="form-control" id="validationCustom03" placeholder="City" value={this.state.address1} onChange={this.handleChange('address1')} required/>
+        {this.state.registered ?
+          <div className="App">You are now registered with Availity!</div>
+        :
+          <form className="needs-validation App" noValidate>
+            <div className="instructions">Please complete the form below and click submit</div>
+            <div className="form-row">
+              <div className="col-md-4 mb-3">
+                <label htmlFor="validationCustom01">First name</label>
+                <input type="text" className="form-control" id="validationCustom01" placeholder="First name" value={this.state.firstName} onChange={this.handleChange('firstName')} required/>
                 <div className="valid-feedback">
                   Looks good!
                 </div>
                 <div className="invalid-feedback">
-                  Please provide a valid address.
+                  Please provide a valid first name.
                 </div>
               </div>
-              <div className="col-md-6 mb-3">
-              <label htmlFor="validationCustom03">Address 2</label>
-                <input type="text" className="form-control" id="validationCustom03" placeholder="City" value={this.state.address2} onChange={this.handleChange('address2')} required/>
+              <div className="col-md-4 mb-3">
+                <label htmlFor="validationCustom02">Last name</label>
+                <input type="text" className="form-control" id="validationCustom02" placeholder="Last name" value={this.state.lastName} onChange={this.handleChange('lastName')} required/>
                 <div className="valid-feedback">
                   Looks good!
                 </div>
                 <div className="invalid-feedback">
-                  Please provide a valid address.
+                  Please provide a valid last name.
                 </div>
               </div>
-          </div>
-          <div className="form-row">
-            <div className="col-md-6 mb-3">
-              <label htmlFor="validationCustom04">City</label>
-              <input type="text" className="form-control" id="validationCustom03" placeholder="City" value={this.state.city} onChange={this.handleChange('city')} required/>
-              <div className="valid-feedback">
-                Looks good!
-              </div>
-              <div className="invalid-feedback">
-                Please provide a valid city.
-              </div>
-            </div>
-            <div className="col-md-3 mb-3">
-              <label htmlFor="validationCustom05">State</label>
-              <input type="text" className="form-control" id="validationCustom04" placeholder="State" value={this.state.homeState} onChange={this.handleChange('homeState')} required/>
-              <div className="valid-feedback">
-                Looks good!
-              </div>
-              <div className="invalid-feedback">
-                Please provide a valid state.
+              <div className="col-md-4 mb-3">
+                <label htmlFor="validationCustom03">NPI</label>
+                <input type="text" className="form-control" id="validationCustom02" placeholder="NPI" value={this.state.npi} onChange={this.handleChange('npi')} required/>
+                <div className="valid-feedback">
+                  Looks good!
+                </div>
+                <div className="invalid-feedback">
+                  Please provide a valid NPI.
+                </div>
               </div>
             </div>
-            <div className="col-md-3 mb-3">
-              <label htmlFor="validationCustom06">Zip</label>
-              <input type="text" className="form-control" id="validationCustom05" placeholder="Zip" value={this.state.zip} onChange={this.handleChange('zip')} required/>
-              <div className="valid-feedback">
-                Looks good!
+            <div className="form-row">
+                <div className="col-md-6 mb-3">
+                <label htmlFor="validationCustom03">Address 1</label>
+                  <input type="text" className="form-control" id="validationCustom03" placeholder="City" value={this.state.address1} onChange={this.handleChange('address1')} required/>
+                  <div className="valid-feedback">
+                    Looks good!
+                  </div>
+                  <div className="invalid-feedback">
+                    Please provide a valid address.
+                  </div>
+                </div>
+                <div className="col-md-6 mb-3">
+                <label htmlFor="validationCustom03">Address 2</label>
+                  <input type="text" className="form-control" id="validationCustom03" placeholder="City" value={this.state.address2} onChange={this.handleChange('address2')} required/>
+                  <div className="valid-feedback">
+                    Looks good!
+                  </div>
+                  <div className="invalid-feedback">
+                    Please provide a valid address.
+                  </div>
+                </div>
+            </div>
+            <div className="form-row">
+              <div className="col-md-6 mb-3">
+                <label htmlFor="validationCustom04">City</label>
+                <input type="text" className="form-control" id="validationCustom03" placeholder="City" value={this.state.city} onChange={this.handleChange('city')} required/>
+                <div className="valid-feedback">
+                  Looks good!
+                </div>
+                <div className="invalid-feedback">
+                  Please provide a valid city.
+                </div>
               </div>
-              <div className="invalid-feedback">
-                Please provide a valid zip.
+              <div className="col-md-3 mb-3">
+                <label htmlFor="validationCustom05">State</label>
+                <input type="text" className="form-control" id="validationCustom04" placeholder="State" value={this.state.homeState} onChange={this.handleChange('homeState')} required/>
+                <div className="valid-feedback">
+                  Looks good!
+                </div>
+                <div className="invalid-feedback">
+                  Please provide a valid state.
+                </div>
+              </div>
+              <div className="col-md-3 mb-3">
+                <label htmlFor="validationCustom06">Zip</label>
+                <input type="text" className="form-control" id="validationCustom05" placeholder="Zip" value={this.state.zip} onChange={this.handleChange('zip')} required/>
+                <div className="valid-feedback">
+                  Looks good!
+                </div>
+                <div className="invalid-feedback">
+                  Please provide a valid zip.
+                </div>
               </div>
             </div>
-          </div>
-          <button className="btn btn-primary" type="submit" onClick={this.onSubmit}>Submit</button>
-        </form>
+            <button className="btn btn-primary button" type="submit" >Submit</button>
+          </form>
+        }
+        <Route exact path="/sub" />
       </div>
     );
   }
---
+++ client/src/pages/Submitted.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Submitted extends Component {
+    render() {
+        return(
+            <div>Hi</div>
+        )
+    }
+}
+
+export default Submitted;
